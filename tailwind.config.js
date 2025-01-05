457343b5eec0ea7a72f92a02b8f81de1752171bc
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-05 15:16:50 +0100

    add: card

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+
+    fontFamily: {
+      'custom': ['Urbanist', 'sans-serif'],
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
