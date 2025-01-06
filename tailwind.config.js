de43ad371fa9a8c8226efd6b4f9d8bc690ed2b39
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-06 17:05:34 +0100

    add:header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,4 +13,16 @@ module.exports = {
     require("flyonui"),
     require("flyonui/plugin")
   ],
+
+  // FlyonUI config (optional - here are the default values)
+  flyonui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "soft"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: false, // include FlyonUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    vendors: false, // default is false when true add customize css for apexChart, editor.js, flatpickr, fullcalendar, notyf, raty-js
+    logs: true, // Shows info about FlyonUI version and used config in the console when building your CSS
+    themeRoot: ":root" // The element that receives theme color CSS variables
+  }
 }
